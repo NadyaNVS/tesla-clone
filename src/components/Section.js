597cac8b6e45c16cd1d4da3e2900f87630c49341
Nadezhda Strelnikova <nvs.strelnikova@gmail.com>
2023-11-24 18:48:30 +0300

    add section

---
+++ src/components/Section.js
@@ -0,0 +1,92 @@
+import React from "react";
+import styled from "styled-components";
+
+function Section({
+  title,
+  description,
+  backgroundImg,
+  leftBtnText,
+  rightBtnText,
+}) {
+  return (
+    <Wrap bgImage={backgroundImg}>
+      <ItemText>
+        <h1>{title}</h1>
+        <p>{description}</p>
+      </ItemText>
+
+      <Buttons>
+        <ButtonGroup>
+          <LeftButton>{leftBtnText}</LeftButton>
+          {rightBtnText && <RightButton>{rightBtnText}</RightButton>}
+        </ButtonGroup>
+        <DownArrow src="images/down-arrow.svg" alt="" />
+      </Buttons>
+    </Wrap>
+  );
+}
+
+const Wrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  width: 100vw;
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  //   background-image: url("/images/model-s.jpg");
+  background-image: ${(props) => `url('/images/${props.bgImage}')`};
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const Buttons = styled.div``;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+  gap: 16px;
+
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  color: white;
+  height: 40px;
+  width: 256px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+  transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
+
+  &:hover {
+    transform: scale(1.05);
+  }
+`;
+
+const RightButton = styled(LeftButton)`
+  background-color: white;
+  opacity: 0.65;
+  color: black;
+`;
+
+const DownArrow = styled.img`
+  height: 40px;
+  //   overflow: hidden;
+  animation: animateDown infinite 1.5s;
+`;
+
+export default Section;
